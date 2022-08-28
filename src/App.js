7a5affdd80ee6e9e014b931de58e3411a23caf54
MayraMar <ing.mayramartinez@gmail.com>
2022-08-27 23:37:08 -0300

    1st working version

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@
 // MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
 // PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
 
+import { useState } from "react";
+import Cabecera from "./components/Cabecera";
+import Listado from "./components/Listado";
+
 function App() {
+  const [cantidad, setCantidad] = useState(0);
+
+  function aumentarCantidad() {
+    setCantidad(cantidad + 1);
+  }
 
   return (
     <div className="App">
-      <Cabecera />
-      <Listado />
+      <Cabecera cantProductos={cantidad} />
+      <Listado sumarUnProducto={aumentarCantidad}/>
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,4 +1,3 @@
-
 // El componente Item no tiene componentes hijos.
 // ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
 // MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
@@ -8,12 +7,31 @@
 //    p
 //    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
 //    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
+import { useState } from "react";
+export default function Item(props) {
+  const [stock, setStock] = useState(props.stock);
 
-export default function Item() {
-
+  function restarStockYSumarProducto(e) {
+    if (stock > 1) {
+      setStock(stock - 1);
+      props.sumarUnProducto();
+    } else if (stock === 1) {
+      setStock("agotado");
+      props.sumarUnProducto();
+      let boton = e.target;
+      boton.innerHTML = "SIN STOCK";
+      boton.disabled = true;
+    }
+  }
+  
   return (
-    <div className='producto'>
-      {/* maquetar Item aquí */}
+    <div className="producto">
+      <h3>{props.producto.nombre}</h3>
+      <p>{props.producto.descripcion}</p>
+      <h5>
+        En stock: <span>{stock}</span>
+      </h5>
+      <button onClick={restarStockYSumarProducto}>COMPRAR</button>
     </div>
-  )
+  );
 }
